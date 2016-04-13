ffab4c8fa9ba2b1c9a4746c769b77743a957e604
Alice <alicewang54@gmail.com>
2016-04-13 04:23:33 -0400

    need to add collision boxes in live 2d

---
+++ Assets/touchController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using live2d;
+using live2d.framework;
+
+
+public class touchController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("hi test");
+
+       LAppModel model = this.gameObject.GetComponent<LAppModelProxy>().GetModel();
+        Debug.Log(model);
+
+        model.SetExpression("f08");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
